d3049a9be6b3eb50a9d49b772111f163e5763cde
AltDel <ic0541800@students.mclennan.edu>
2021-10-10 18:39:48 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,27 +1,27 @@
 var thevideo = document.getElementById("punchbox");
-$.getJSON('https://ipinfo.io', function(data) {
-  var asia_arr = ['AE', 'AF', 'AM', 'AZ', 'BD', 'BH', 'BN', 'BT', 'CC', 'CN', 'CX'
-  , 'CY', 'GE', 'HK', 'IL', 'IN', 'IO', 'IQ', 'IR', 'JO', 'JP', 'KG', 'KH', 'KP', 'KR', 'KW',
-  , 'KZ', 'LA', 'LB', 'LK', 'MM', 'MN', 'MO', 'MV', 'MY', 'NP', 'OM', 'PH', 'PK', 'PS', 'QA', 'SA'
-  , 'SG', 'SY', 'TH', 'TJ', 'TM', 'TR', 'TW', 'UZ', 'VN', 'YE'];
-  
-  var europe_arr = ['AD', 'AL', 'AT', 'BA', 'BE', 'BG', 'BY', 'CH', 'CZ', 'DE', 'DK', 'EE', 'ES', 'FI', 'FO', 'FR', 'GB', 'GG'
-  , 'GI', 'GR', 'HR', 'HU', 'IE', 'IM', 'IS', 'IT', 'JE', 'LI', 'LT', 'LU', 'LV', 'MC', 'MD', 'ME', 'MK'
-  , 'MT', 'NL', 'NO', 'PL', 'PT', 'RO', 'RS', 'RU', 'SE', 'SI', 'SJ', 'SK', 'SM', 'UA'];
-  var america_arr = ['CA', 'US', 'MX'];
-  var oceania_arr = ['AU', 'NZ', 'ID'];
-  function checkValue(value, arr) {
-    var status = 'Not exist';
-    for (var i = 0; i < arr.length; i++) {
-          var name = arr[i];
-          if (name == value) {
-               status = 'Exist';
-               break;
-          }
-    }
+var asia_arr = ['AE', 'AF', 'AM', 'AZ', 'BD', 'BH', 'BN', 'BT', 'CC', 'CN', 'CX'
+, 'CY', 'GE', 'HK', 'IL', 'IN', 'IO', 'IQ', 'IR', 'JO', 'JP', 'KG', 'KH', 'KP', 'KR', 'KW',
+, 'KZ', 'LA', 'LB', 'LK', 'MM', 'MN', 'MO', 'MV', 'MY', 'NP', 'OM', 'PH', 'PK', 'PS', 'QA', 'SA'
+, 'SG', 'SY', 'TH', 'TJ', 'TM', 'TR', 'TW', 'UZ', 'VN', 'YE'];
 
-      return status;
-   }
+var europe_arr = ['AD', 'AL', 'AT', 'BA', 'BE', 'BG', 'BY', 'CH', 'CZ', 'DE', 'DK', 'EE', 'ES', 'FI', 'FO', 'FR', 'GB', 'GG'
+, 'GI', 'GR', 'HR', 'HU', 'IE', 'IM', 'IS', 'IT', 'JE', 'LI', 'LT', 'LU', 'LV', 'MC', 'MD', 'ME', 'MK'
+, 'MT', 'NL', 'NO', 'PL', 'PT', 'RO', 'RS', 'RU', 'SE', 'SI', 'SJ', 'SK', 'SM', 'UA'];
+var america_arr = ['CA', 'US', 'MX'];
+var oceania_arr = ['AU', 'NZ', 'ID'];
+function checkValue(value, arr) {
+  var status = 'Not exist';
+  for (var i = 0; i < arr.length; i++) {
+        var name = arr[i];
+        if (name == value) {
+             status = 'Exist';
+             break;
+        }
+  }
+
+    return status;
+}
+$.getJSON('https://ipinfo.io', function(data) {
    if (checkValue(data.country, asia_arr) == "Exist") {
        thevideo.src = "http://13.230.2.76:8080/media/boxingreencode.mp4";
        document.getElementById("thistext").innerHTML = "Server: AWS Lightsail, Tokyo";
